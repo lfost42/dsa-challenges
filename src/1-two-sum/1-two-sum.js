91eaf41d0341803ab44bf558bfcefd89a93142e0
Lynda Foster <lfost42@wgu.edu>
2022-03-16 20:31:00 -0700

    fix

--- src/1-two-sum/1-two-sum.js
+++ src/1-two-sum/1-two-sum.js
@@ -1,7 +1,7 @@
 const twoSum = (array, goal) => {
   let numberMap = new Map();
   for (let index = 0; index < array.length; index++) {
-    el = array[index];
+    let el = array[index];
     if (numberMap.has(goal - el))
       return [index, numberMap.get(goal - el)];
     else numberMap.set(el, index);
