8973a9ae44bc3440f34c7e619e74ce46f6a82071
Lynda Foster <lfost42@wgu.edu>
2022-03-28 22:12:32 -0700

    completed Arrays I

--- prep/arrays/22-my-reverse.js
+++ prep/arrays/22-my-reverse.js
@@ -5,3 +5,14 @@
 
 //Do not use the built-in .reverse method for this problem. You can use it on all 
 //future problems though!
+
+function myReverse(myArray) {
+  let newArray = [];
+  for (let i = myArray.length-1; i >=0; i--) {
+    newArray.push(myArray[i]);
+  }
+  return newArray;
+}
+
+//We initiate an empty newArray and add the numbers
+//from the input array starting at the end to the beginning.
--- prep/arrays/23-my-unshift.js
+++ prep/arrays/23-my-unshift.js
@@ -6,3 +6,15 @@
 //myUnshift([1, 2, 3], 0); // => [0, 1, 2, 3];
 
 //Do not use the built-in .unshift method for this problem!
+
+function myUnshift(inputArray, inputValue) {
+  let newArray = [];
+  newArray.push(inputValue);
+  for (let i = 0; i < inputArray.length; i++) {
+    newArray.push(inputArray[i]);
+  }
+  return newArray;
+}
+
+//We start by initiating newArray to an empty array and push inputValue into
+//the array. The for loop then pushes the the inputArray onto newArray. 
