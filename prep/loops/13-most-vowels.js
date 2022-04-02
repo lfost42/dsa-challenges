b238e8d2d1a7fb653fab200e1db5570600f7c9d7
Lynda Foster <lfost42@wgu.edu>
2022-04-02 13:46:12 -0700

    Update 13-most-vowels.js

--- prep/loops/13-most-vowels.js
+++ prep/loops/13-most-vowels.js
@@ -6,26 +6,53 @@
 //If none of the words have any vowels, return an empty string.
 //mostVowels('try my gym'); // => ''
 
-function mostVowels(words) {
-  let wordArray = [];
-  wordArray = words.split(' ');  
+// function mostVowels(words) {
+//   let wordArray = [];
+//   wordArray = words.split(' ');  
   
-  let max = wordArray[0];
-  for (let i = 0; i < wordArray.length; i++) {
-    let current = wordArray[i];
-    if (Array.from(wordArray[i]).filter(letter => 'aeiou'.includes(letter)).length > Array.from(wordArray[0]).filter(letter => 'aeiou'.includes(letter)).length) {
-      max = current;
-    }
-  }
-  if (Array.from(max).filter(letter => 'aeiou'.includes(letter)).length !== 0) {
-      return max;
-    } else {
-      return '';
-    }
-}
-
+//   let max = wordArray[0];
+//   for (let i = 0; i < wordArray.length; i++) {
+//     let current = wordArray[i];
+//     if (Array.from(wordArray[i]).filter(letter => 'aeiou'.includes(letter)).length > Array.from(wordArray[0]).filter(letter => 'aeiou'.includes(letter)).length) {
+//       max = current;
+//     }
+//   }
+//   if (Array.from(max).filter(letter => 'aeiou'.includes(letter)).length !== 0) {
+//       return max;
+//     } else {
+//       return '';
+//     }
+// }
 //We first split the words into an array and initiate the variable max to contain the 
 //first word in the new array. Then we iterate through each word while counting how many 
 //vowels are in each. When the loop discovers a word with more vowels than max, it is 
 //replaced with that word. Finally, we check whether max has vowels and return an empty 
 //string if it does not. Otherwise we return the word.
+
+function mostVowels(sentence) {
+  let vowels = "aeiouAEIOU";
+  let str = "";
+  let mostVowels = 0;
+  let currentWord = "";
+  let currentVowel = 0;
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] === " " || i === sentence.length-1) {
+      if (mostVowels < currentVowel) {
+        mostVowels = currentVowel;
+        str = currentWord;
+      }
+      currentWord = "";
+      currentVowel = 0;
+    } else {
+      currentWord += sentence[i];
+      if (vowels.includes(sentence[i])) {
+        currentVowel++;
+      }
+    }
+  }
+  return str;
+}
+/*
+First we create variables for str, mostVowels, currentWord (index), and currentVowel (index). 
+A for loop iterates through each word in the sentence. 
+*/
