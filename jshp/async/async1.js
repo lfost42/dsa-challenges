f144c65eaf5a0a4cecf3d7ea97b717479e65554e
Lynda Foster <lfost42@wgu.edu>
2022-05-23 21:48:39 -0700

    async

--- jshp/async/async1.js
+++ jshp/async/async1.js
@@ -1,5 +1,22 @@
-/*
-*/
+function printHello() {
+	console.log("Hello");
+}
+
+setTimeout(printHello, 1000);
+
+console.log("Me first!");
 
 /*
+Line 1: Declare a function definition with the label PrintHello and store in in global memory.
+Line 5: Invoke setTimeout and open an execution context. 
+	setTimeout is pushed onto the call stack. In local memory we pair the arguments with the parameters. The first parameter is the function definition printHello and the second parameter is 1000. 
+	setTimeout pushes the function definition and 1000 to the Web Browser API and immediately starts a timer. Once pushed to the web browser, the thread of execution goes back to the global execution context and it is available for garbage collection.
+	setTimeout is popped off the call stack. 
+	Line 7. "Me First" is logged to the console. 
+	The thread checks the callback queue and find nothing. Every ms, the thread of execution checks the callback queue and loops until something appears (event loop). 
+	When the 1000ms has lapsed, the function definition is pushed to the callback queue. 
+	When the event loop finds it, the reference to the execution context is pushed to the call stack and a new execution context is opened. 
+	Line 2: "Hello" is logged to the console. 
+	We cehck the callback queue and find nothing.
+	The setTimer is popped off the call stack, the execution context is garbage collected. 
 */
--- jshp/async/async2.js
+++ jshp/async/async2.js
@@ -1,5 +1,29 @@
-/*
-*/
+function printHello() {
+	console.log("Hello");
+}
+
+function blockFor1Sec() {
+
+}
+
+setTimeout(printHello, 0);
+
+blockFor1Sec();
+
+console.log("Me first!")
 
 /*
+Line 1: define a function with the label printHello and store in global memory.
+Line 5: define a function with the label blokfor1Se and store it in global memory. 
+Line 9: invoke setTimeout with parameters printHello and 0.
+	setTimeout is pushed to the call stack and opens a new execution context. 
+	The function definition printHello and 0 are stored in local memory. 
+	The function definition and time is pushed to the web browser API to enqueue to the callback queue after at least 0 ms.
+	The function definition is enqueued to the callback queue. 
+	setTimeout is popped off the call stack and the execution context is available for gc.
+Line 11: blockFor1Sec is invoked. A local execution context is opened and pushed to the call stack. 
+After approx 1000ms, the thread of execution leaves the execution context and is available for gc and the function is popped off the call stack. 
+Line 13: "Me first" is printed to the console and the event loop checks the callback queue and finds the function definition for printHello and is pushed to the callStack.
+*The event loop had to wait until blockFor1Sec was popped off the call stack before it can allow the callback queue to proceed.
+	A new execution context is open and printHello is pushed to the call stack. The console logs "Hello". The function is popped off the call stack and the execution context is popped off the call stack. 
 */
