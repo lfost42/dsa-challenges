1b0f85820eef1844771755c345e0e8f8683bb851
Lynda Foster <lfost42@wgu.edu>
2022-06-07 19:28:55 -0700

    Update 7-intersection.js

--- leetjr/arraandstring/7-intersection.js
+++ leetjr/arraandstring/7-intersection.js
@@ -1,6 +1,8 @@
 /* PROBLEM */
 /*
-Write a function, intersection, that takes in two arrays, a,b, as arguments. The function should return a new array containing elements that are in both of the two arrays.
+Write a function, intersection, that takes in two arrays, a,b, as arguments. 
+The function should return a new array containing elements that are in both of 
+the two arrays.
 
 You may assume that each input array does not contain duplicate elements.
 
@@ -53,7 +55,11 @@ const intersection = (a, b) => {
 
 /* EXPLANATION */
 /*
-We declare a variable result and instantiate it to the value of an empty array. We declare another variable items and instantiate it to the value of a set. In a for loop we add each item of a into the set.
+We declare a variable result and instantiate it to the value of an empty array. 
+We declare another variable items and instantiate it to the value of a set. In 
+a for loop we add each item of a into the set.
 
-The second loop iterates through each element of b. Then we check if the item *has* the element (which is O(1) run time) and push the element into the result array. 
+The second loop iterates through each element of b. Then we check if the item 
+*has* the element (which is O(1) run time) and push the element into the result 
+array. 
 */
