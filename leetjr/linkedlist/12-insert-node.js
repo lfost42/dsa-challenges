53f02b7c0594936255a0b04e2fa29612c6c28160
Lynda Foster <lfost42@wgu.edu>
2022-06-09 19:49:24 -0700

    insert node

--- leetjr/linkedlist/12-insert-node.js
+++ leetjr/linkedlist/12-insert-node.js
@@ -65,8 +65,38 @@ insertNode(a, 'z', 0);
 */
 
 /* SOLUTION */
-This is another test
+const insertNode = (head, value, index) => {
+	if (index === 0) {
+		const newHead = new Node(value);
+		newHead.next = head;
+		return newHead;
+	}
+
+	let count = 0;
+	let current = head;
+	while (current !== null) {
+		if (count === index - 1) {
+			const next = current.next;
+			current.next = new Node(value);
+			current.next.next = next;
+		}
+		count++;
+		current = current.next;
+	}
+	return head;
+};
 
 /* EXPLANATION*/
 /*
+Our function will take in three parameters as arguments: head, value, and index. Our function will add a node(value) at the index provided. We will then return the head of the new linked list. 
+
+Generally, we are adding the node at index - 1. However, if index were 0, we would be out of bounds. So we'll start by checking if index is 0 and if it is, we'll declare a constant newHead and assign it to a new Node with the value given. Then we point newHead.next to head and return newHead.
+
+We declare a count variable and assign it to 0. We ceclare a current variable and assign it to head. We begin a while loop to execute while current is not null. 
+
+We declare a temp variable and assign it current.next. This allows us to assign current.next to the new node value (inserts the new node). We complete the link by assigning current.next.next to temp.
+
+Outside of the loop, we increment count and traverse the linked list by assigning current to current.next
+
+Finally, we return head. 
 */
