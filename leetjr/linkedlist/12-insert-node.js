3ced0f3fd32f634d32bb3c4ae1570ce0f988d3f4
Lynda Foster <lfost42@wgu.edu>
2022-06-09 22:30:49 -0700

    Update 12-insert-node.js

--- leetjr/linkedlist/12-insert-node.js
+++ leetjr/linkedlist/12-insert-node.js
@@ -1,10 +1,15 @@
 /* PROBLEM */
 /*
-Write a function, insertNode, that takes in the head of a linked list, a value, and an index. The function should insert a new node with the value into the list at the specified index. Consider the head of the linked list as index 0. The function should return the head of the resulting linked list.
+Write a function, insertNode, that takes in the head of a linked 
+list, a value, and an index. The function should insert a new 
+node with the value into the list at the specified index. 
+Consider the head of the linked list as index 0. The function 
+should return the head of the resulting linked list.
 
 Do this in-place.
 
-You may assume that the input list is non-empty and the index is not greater than the length of the input list.
+You may assume that the input list is non-empty and the index is 
+not greater than the length of the input list.
 
 test_00:
 
@@ -88,15 +93,28 @@ const insertNode = (head, value, index) => {
 
 /* EXPLANATION*/
 /*
-Our function will take in three parameters as arguments: head, value, and index. Our function will add a node(value) at the index provided. We will then return the head of the new linked list. 
+Our function will take in three parameters as arguments: head, 
+value, and index. Our function will add a node(value) at the 
+index provided. We will then return the head of the new linked 
+list. 
 
-Generally, we are adding the node at index - 1. However, if index were 0, we would be out of bounds. So we'll start by checking if index is 0 and if it is, we'll declare a constant newHead and assign it to a new Node with the value given. Then we point newHead.next to head and return newHead.
+Generally, we are adding the node at index - 1. However, if index 
+were 0, we would be out of bounds. So we'll start by checking if 
+index is 0 and if it is, we'll declare a constant newHead and 
+assign it to a new Node with the value given. Then we point 
+newHead.next to head and return newHead.
 
-We declare a count variable and assign it to 0. We ceclare a current variable and assign it to head. We begin a while loop to execute while current is not null. 
+We declare a count variable and assign it to 0. We ceclare a 
+current variable and assign it to head. We begin a while loop to 
+execute while current is not null. 
 
-We declare a temp variable and assign it current.next. This allows us to assign current.next to the new node value (inserts the new node). We complete the link by assigning current.next.next to temp.
+We declare a temp variable and assign it current.next. This 
+allows us to assign current.next to the new node value (inserts 
+the new node). We complete the link by assigning 
+current.next.next to temp.
 
-Outside of the loop, we increment count and traverse the linked list by assigning current to current.next
+Outside of the loop, we increment count and traverse the linked 
+list by assigning current to current.next
 
 Finally, we return head. 
 */
