e7a7f330d7e6e7b4a819ac9aa0ea41b9ec0b7d17
Lynda Foster <lfost42@wgu.edu>
2022-06-09 22:29:59 -0700

    Update 13-create-linked-list.js

--- leetjr/linkedlist/13-create-linked-list.js
+++ leetjr/linkedlist/13-create-linked-list.js
@@ -1,6 +1,9 @@
 /* PROBLEM */
 /*
-Write a function, createLinkedList, that takes in an array of values as an argument. The function should create a linked list containing each element of the array as the values of the nodes. The function should return the head of the linked list.
+Write a function, createLinkedList, that takes in an array of 
+values as an argument. The function should create a linked list 
+containing each element of the array as the values of the nodes. 
+The function should return the head of the linked list.
 
 test_00:
 
@@ -33,9 +36,17 @@ const createLinkedList = (values) => {
 
 /* EXPLANATION*/
 /*
-Our function takes an array as an argument and creates a linked list containing each element of the array. The function returns the head of the linked list. 
+Our function takes an array as an argument and creates a linked 
+list containing each element of the array. The function returns 
+the head of the linked list. 
 
-We first declare a constant dummyHead and assign it to a new null node. We declare a variable tail and assign it to dummy head. This begins our linked list. Then we iterate through the array for each element of values. We assign tail.next to a new node containing the value of the element of the array and we assign tail to tail.next (to shift the linked list to the new tail at every loop.)
+We first declare a constant dummyHead and assign it to a new null 
+node. We declare a variable tail and assign it to dummy head. 
+This begins our linked list. Then we iterate through the array 
+for each element of values. We assign tail.next to a new node 
+containing the value of the element of the array and we assign 
+tail to tail.next (to shift the linked list to the new tail at 
+every loop.)
 
 We return dummyHead.next (dummyHead is null). 
 */
