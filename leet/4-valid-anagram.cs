0faebc06f72b83e4f2cbf4913e01b85be35a4f6f
Lynda Foster <lfost42@wgu.edu>
2022-06-11 08:00:19 -0700

    valid anagram c#

--- leet/4-valid-anagram.cs
+++ leet/4-valid-anagram.cs
@@ -25,11 +25,39 @@ s and t consist of lowercase English letters.
 /* SOLUTION */
 public class Solution {
     public bool IsAnagram(string s, string t) {
+        Dictionary<char, int> count = new();
 
+        foreach(char ltr in s
+                    .ToLower()
+                    .ToCharArray()) {
+            if (count.ContainsKey(ltr)) {
+                count[ltr]++;
+            } else {
+                count.Add(ltr, 1);
+            }
+        }
+
+        foreach (char ltr in t
+                    .ToLower()
+                    .ToCharArray()) {
+            if (!count.ContainsKey(ltr)) {
+            return false;
+            } else if(--count[ltr]==0) {
+                count.Remove(ltr);
+            }
+        }
+        return count.Count == 0;
     }
 }
 /* EXPLANATION*/
 /*
 Our function accepts two strings as arguments. We return true if the words are anagrams and false if they are not. 
 
+We declare a new dictionary that accepts a char as a key and integer as a value. We assign it a label count. 
+
+We loop through each ltr in string s, we set it to lowercase if it isn't, and convert it to a character array. If the ltr is a key, we increment the value, otherwise we add the ltr and assign it a value of 1. 
+
+We loop through each ltr in string t, we set it to lowercase if it isn't, and convert it to a character array. If the ltr is not a key, we return false because it is not an anagram. If the value is less than 0 after decrementing, we remove the ltr from the dictionary. 
+
+We return a boolean true if there are no values left in our dictionary and false if any remains. 
 */
