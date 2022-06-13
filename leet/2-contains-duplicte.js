d293a2822acae4aa26aeaa8bf2c2c3e31cf927bf
Lynda Foster <lfost42@wgu.edu>
2022-06-12 21:37:34 -0700

    Update 2-contains-duplicte.js

--- leet/2-contains-duplicte.js
+++ leet/2-contains-duplicte.js
@@ -1,6 +1,8 @@
 /* PROBLEM */
 /*
-Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+Given an integer array nums, return true if any value appears at 
+least twice in the array, and return false if every element is 
+distinct.
 
 Example 1:
 
@@ -37,7 +39,13 @@ var containsDuplicate = function (nums) {
 
 /* EXPLANATION*/
 /*
-Our function will accept an array nums as an argument and return true if any value appears at least twice in teh array and false if not. 
+Our function will accept an array nums as an argument and return 
+true if any value appears at least twice in teh array and false 
+if not. 
 
-We declare a constant prevNums and assign it equal to an empty set. We loop through each element in nums. Our conditional checks if prevNums has the element and returns true if it does, otherwise it adds the element to the set. If we exit the loop, we did not find duplicates and we return false. 
+We declare a constant prevNums and assign it equal to an empty 
+set. We loop through each element in nums. Our conditional checks 
+if prevNums has the element and returns true if it does, 
+otherwise it adds the element to the set. If we exit the loop, 
+we did not find duplicates and we return false. 
 */
