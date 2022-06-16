a0df5072a0efc53c688b86da1895e997163ffb43
Lynda Foster <lfost42@wgu.edu>
2022-06-16 08:00:00 -0700

    Update 8-3sum.cs

--- leet/8-3sum.cs
+++ leet/8-3sum.cs
@@ -1,6 +1,8 @@
 /* PROBLEM */
 /*
-Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+Given an integer array nums, return all the triplets [nums[i], 
+nums[j], nums[k]] such that i != j, i != k, and j != k, and 
+nums[i] + nums[j] + nums[k] == 0.
 
 Notice that the solution set must not contain duplicate triplets.
 
@@ -67,9 +69,23 @@ public class Solution {
 
 /* EXPLANATION*/
 /*
-We first sort the input nums array. We declare a constant triplets and assign it equal to an empty integer array. We loop through the elements except the last (the last element will be our right pointer).
+We first sort the input nums array. We declare a constant 
+triplets and assign it equal to an empty integer array. We loop 
+through the elements except the last (the last element will be 
+our right pointer).
 
-We begin a for loop to iterate while left is less than right. We begin a conditional that checks each element is not equal to the previous element. We declare the left pointer and set it equal to i + 1. We declare the right pointer and set it equal to the last index in the array. 
+We begin a for loop to iterate while left is less than right. We 
+begin a conditional that checks each element is not equal to the 
+previous element. We declare the left pointer and set it equal 
+to i + 1. We declare the right pointer and set it equal to the 
+last index in the array. 
 
-We declare a constant currentSum to equal the sum of nums[i], nums[left], and nums[right]. Every time currentSum is equal to 0 (our target), we push the current elements into the triplets array. While nums[left] is equal to nums[left + 1], we increment left. While nums[right] is equal to nums[right - 1] we decrement right. We increment left and decrement right. If currentSum is less than 0, we increment left. If currentSum is greater than 0, we decrement right. 
+We declare a constant currentSum to equal the sum of nums[i], 
+nums[left], and nums[right]. Every time currentSum is equal to 0 
+(our target), we push the current elements into the triplets 
+array. While nums[left] is equal to nums[left + 1], we increment 
+left. While nums[right] is equal to nums[right - 1] we decrement 
+right. We increment left and decrement right. If currentSum is 
+less than 0, we increment left. If currentSum is greater than 0, 
+we decrement right. 
 */
