443011f67e0eb12db5f447fa49ff9ce81a523a66
Lynda Foster <lfost42@wgu.edu>
2022-09-03 14:14:52 -0700

    Update 4-last-character.js

--- javascript/prep/functions/4-last-character.js
+++ javascript/prep/functions/4-last-character.js
@@ -1,7 +1,11 @@
-//Define a function lastCharacter that accepts two strings as arguments. lastCharacter should return true if both strings end with the same character. Otherwise, lastCharacter should return false.
+//Define a function lastCharacter that accepts two strings as 
+arguments. lastCharacter should return true if both strings end 
+with the same character. Otherwise, lastCharacter should return 
+false.
 
 function lastCharacter(str1, str2) {
   return str1.slice(-1) === str2.slice(-1);
 }
 
-// slice(-1) is a string method that checks the last character of a string. 
+// slice(-1) is a string method that checks the last character of 
+a string. 
