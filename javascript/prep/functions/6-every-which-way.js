9af93cb089a6a2e17e28de8730ba71e818eda787
Lynda Foster <lfost42@wgu.edu>
2022-09-03 14:16:06 -0700

    Update 6-every-which-way.js

--- javascript/prep/functions/6-every-which-way.js
+++ javascript/prep/functions/6-every-which-way.js
@@ -1,8 +1,13 @@
-//Write a function everyWhichWay that accepts three number arguments. everyWhichWay should:
-//return 'sum' if the sum of the first two numbers equals the third
-//return 'difference' if the first number minus the second equals the third
-//return 'product' if the product of the first two numbers equals the third
-//return 'fraction' if the first number divided by the second equals the third
+//Write a function everyWhichWay that accepts three number 
+arguments. everyWhichWay should:
+//return 'sum' if the sum of the first two numbers equals the 
+third
+//return 'difference' if the first number minus the second equals 
+the third
+//return 'product' if the product of the first two numbers equals 
+the third
+//return 'fraction' if the first number divided by the second 
+equals the third
 //return null if none of the above are true
 
 function everyWhichWay(num1, num2, num3) {
