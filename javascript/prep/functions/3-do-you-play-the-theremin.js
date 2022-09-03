13097431894c7990c9a0c7fc615090376bdf0117
Lynda Foster <lfost42@wgu.edu>
2022-09-03 14:14:22 -0700

    Update 3-do-you-play-the-theremin.js

--- javascript/prep/functions/3-do-you-play-the-theremin.js
+++ javascript/prep/functions/3-do-you-play-the-theremin.js
@@ -1,9 +1,12 @@
-//Define a function doYouPlayTheTheremin that accepts a string as an argument. 
-//doYouPlayTheTheremin should return true if the inputted string starts with the letters s or S.
+//Define a function doYouPlayTheTheremin that accepts a string as 
+an argument. 
+//doYouPlayTheTheremin should return true if the inputted string 
+starts with the letters s or S.
 //Otherwise, doYouPlayTheTheremin should return false.
 
 function doYouPlayTheTheremin(str) {
   return str[0].toLowerCase() === "s";
 }
 
-//The logical operation sets the string to lower case and checks if it starts with 's'.
+//The logical operation sets the string to lower case and checks 
+if it starts with 's'.
