aac87ea30a510c6a512cec4a9970fb7b2a91c772
Lynda Foster <lfost42@wgu.edu>
2022-09-03 14:09:41 -0700

    Update 1-greeting.js

--- javascript/prep/functions/1-greeting.js
+++ javascript/prep/functions/1-greeting.js
@@ -1,8 +1,12 @@
-//Define a function greeting that accepts an optional string argument called name. greeting should return a personalized string if the name is present.
+//Define a function greeting that accepts an optional string 
+argument called name. greeting should return a personalized 
+string if the name is present.
 
 function greeting(name) {
   let greeting = 'Hello';
   return greeting + (name !== undefined ? `${name}!` : '!')
 }
 
-//The string will return 'Hello name!' if the greeting includes a name and just 'Hello!' if the name is undefined (instead of 'Hello undefined!').
+//The string will return 'Hello name!' if the greeting includes 
+a name and just 'Hello!' if the name is undefined (instead of 
+'Hello undefined!').
