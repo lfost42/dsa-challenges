977ffbec8536866e13097d4ebd7de91da771aa94
Lynda Foster <lfost42@wgu.edu>
2022-09-12 20:04:28 -0700

    Update 7-most-vowels.js

--- javascript/prep/loops/7-most-vowels.js
+++ javascript/prep/loops/7-most-vowels.js
@@ -1,10 +1,12 @@
-//Define a function, mostVowels, that accepts one argument, a string of words.
+/*Define a function, mostVowels, that accepts one argument, a 
+string of words.
 
-//mostVowels should return the word that has the most vowels.
-//mostVowels('I am a keeper with some real rhythms'); // => keeper
+mostVowels should return the word that has the most vowels. 
+mostVowels('I am a keeper with some real rhythms'); // => keeper
 
-//If none of the words have any vowels, return an empty string.
-//mostVowels('try my gym'); // => ''
+If none of the words have any vowels, return an empty 
+string.mostVowels('try my gym'); // => ''
+*/
 
 // function mostVowels(words) {
 //   let wordArray = [];
@@ -23,11 +25,16 @@
 //       return '';
 //     }
 // }
-//We first split the words into an array and initiate the variable max to contain the 
-//first word in the new array. Then we iterate through each word while counting how many 
-//vowels are in each. When the loop discovers a word with more vowels than max, it is 
-//replaced with that word. Finally, we check whether max has vowels and return an empty 
-//string if it does not. Otherwise we return the word.
+
+/*
+We first split the words into an array and initiate the variable 
+max to contain the first word in the new array. Then we iterate 
+through each word while counting how many vowels are in each. 
+When the loop discovers a word with more vowels than max, it is 
+replaced with that word. Finally, we check whether max has vowels 
+and return an empty string if it does not. Otherwise we return 
+the word.
+*/
 
 function mostVowels(sentence) {
   let vowels = "aeiouAEIOU";
@@ -53,6 +60,7 @@ function mostVowels(sentence) {
   return str;
 }
 /*
-First we create variables for str, mostVowels, currentWord (index), and currentVowel (index). 
-A for loop iterates through each word in the sentence. 
+First we create variables for str, mostVowels, currentWord 
+(index), and currentVowel (index). A for loop iterates through 
+each word in the sentence. 
 */
