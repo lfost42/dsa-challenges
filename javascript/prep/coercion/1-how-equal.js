d0aebb7c112fd14fdf6e57499760a11259d36aa9
Lynda Foster <lfost42@wgu.edu>
2022-09-16 20:20:16 -0700

    Update 1-how-equal.js

--- javascript/prep/coercion/1-how-equal.js
+++ javascript/prep/coercion/1-how-equal.js
@@ -1,14 +1,17 @@
-//Define a function, howEqual, that accepts two arguments of any type.
+/*
+Define a function, howEqual, that accepts two arguments of any 
+type.
 
-//howEqual should return the string 'loosely' if the two values 
-//passed into the function are loosely equal. howEqual should return 
-//'strictly' if the two values are strictly equal. howEqual should 
-//return the string 'not equal' if the two values are neither 
-//strictly nor loosely equal.
+howEqual should return the string 'loosely' if the two values 
+passed into the function are loosely equal. howEqual should 
+return 'strictly' if the two values are strictly equal. howEqual 
+should return the string 'not equal' if the two values are 
+neither strictly nor loosely equal.
 
-//howEqual(0, '0'); // => loosely
-//howEqual(3, 9/3); // => strictly
-//howEqual(true, 'truthy') // => not equal
+howEqual(0, '0'); // => loosely
+howEqual(3, 9/3); // => strictly
+howEqual(true, 'truthy') // => not equal
+*/
 
 function howEqual(arg1, arg2) {
   if (arg1 === arg2) {
@@ -20,6 +23,8 @@ function howEqual(arg1, arg2) {
   }
 }
 
-//strictly checks for value and type
-//loosely checks for value but not type
-//not equal checks neither value nor type
+/*
+strictly checks for value and type
+loosely checks for value but not type
+not equal checks neither value nor type
+*/
