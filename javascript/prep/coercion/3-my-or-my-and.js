7d890fb936ce1b6d8c58ce4f7149460c34c8cbf7
Lynda Foster <lfost42@wgu.edu>
2022-09-18 12:43:25 -0700

    Update 3-my-or-my-and.js

--- javascript/prep/coercion/3-my-or-my-and.js
+++ javascript/prep/coercion/3-my-or-my-and.js
@@ -1,16 +1,22 @@
-//Define a function, myOr, that accepts three arguments of any type.
+/*
+Define a function, myOr, that accepts three arguments of any type.
 
-//myOr should return the same result as the buit-in || operator:
-//myOr(true, true, false) === (true || true || false); // => true
+myOr should return the same result as the buit-in || operator:
+myOr(true, true, false) === (true || true || false); // => true
 
-//Then, define a function, myAnd, that accepts three arguments of any type.
+Then, define a function, myAnd, that accepts three arguments of 
+any type.
 
-//myAnd should return the same result as the buit-in && operator:
-//myAnd(1, 'apples', true) === (1 && 'apples' && true); // => true
+myAnd should return the same result as the buit-in && operator:
+myAnd(1, 'apples', true) === (1 && 'apples' && true); // => true
 
-//Hint: Refer to the two rules for and and or [from this lecture on truthiness and type coercion]
-// && returns the first falsey value, or the last value if all are truthy
-// || returns the first truthy value, or the last value if all are falsey
+Hint: Refer to the two rules for and and or [from this lecture 
+on truthiness and type coercion]
+&& returns the first falsey value, or the last value if all are 
+truthy
+|| returns the first truthy value, or the last value if all are 
+falsey
+*/
 
 const myOr = (arg1, arg2, arg3) => {
   if (arg1) {
@@ -24,7 +30,9 @@ const myOr = (arg1, arg2, arg3) => {
   }
   return arg3;
 }
-//returns the first truthy value, or the last value if all are falsey
+
+/* returns the first truthy value, or the last value if all are 
+falsey */
 
 const myAnd = (arg1, arg2, arg3) => {
   if (!arg1) {
@@ -38,4 +46,6 @@ const myAnd = (arg1, arg2, arg3) => {
   }
   return arg3;
 }
-//returns the first falsey value, or the last value if all are truthy
+
+/* returns the first falsey value, or the last value if all are 
+truthy
