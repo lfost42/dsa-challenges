105616af4390ab22f47be1b25d304f4f19ca6359
Lynda Foster <lfost42@wgu.edu>
2022-10-12 18:52:26 -0700

    Update 1-odd-couple.js

--- javascript/prep/arrays/1-odd-couple.js
+++ javascript/prep/arrays/1-odd-couple.js
@@ -1,12 +1,18 @@
-//Define a function, oddCouple, that accepts an array of numbers as an argument.
+/*
+Define a function, oddCouple, that accepts an array of numbers as 
+an argument.
 
-//oddCouple should return a new array with the first two odd numbers from the original array:
-//oddCouple([1, 2, 3, 4, 5]); // => [1, 3]
+oddCouple should return a new array with the first two odd 
+numbers from the original array:
+oddCouple([1, 2, 3, 4, 5]); // => [1, 3]
 
-//If fewer than two odd numbers exist in the original array, return an array with only the 
-//one odd number, or an empty array if there are no odd numbers:
-//oddCouple([10, 15, 20]); // => [15]
-//oddCouple(2, 4, 6, 8); // => []
+If fewer than two odd numbers exist in the original array, return 
+an array with only the one odd number, or an empty array if there 
+are no odd numbers:
+
+oddCouple([10, 15, 20]); // => [15]
+oddCouple(2, 4, 6, 8); // => []
+*/
 
 function oddCouple(myArray) {
   let newArray = [];
@@ -17,6 +23,9 @@ function oddCouple(myArray) {
     return newArray;
 }
 
-//First create a newArray and initiate it to the value of an empty array.
-//The loop checks whether a number is odd. If the length of newArray equals
-//2, we return the newArray. Else, we return newArray when the loop is complete.
+/* 
+First create a newArray and initiate it to the value of an 
+empty array. The loop checks whether a number is odd. If the 
+length of newArray equals 2, we return the newArray. Else, we 
+return newArray when the loop is complete. 
+*/
