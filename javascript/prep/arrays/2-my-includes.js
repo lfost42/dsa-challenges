11a987930c533590629ca53d1eb7c299337ce8be
Lynda Foster <lfost42@wgu.edu>
2022-10-13 18:51:00 -0700

    Update 2-my-includes.js

--- javascript/prep/arrays/2-my-includes.js
+++ javascript/prep/arrays/2-my-includes.js
@@ -1,10 +1,15 @@
-//Define a function, myIncludes, that accepts an array and a searchValue as arguments.
+/*
+Define a function, myIncludes, that accepts an array and a 
+searchValue as arguments.
 
-//myIncludes should return true if the searchValue is an element in the array. Otherwise, 
-//myIncludes should return false.
+myIncludes should return true if the searchValue is an element in 
+the array. Otherwise, 
+myIncludes should return false.
 
-//Do not use the built-in .includes array method during this problem. Feel free to use it 
-//on any future problem though! Note that strings have an .includes method, too.
+Do not use the built-in .includes array method during this 
+problem. Feel free to use it on any future problem though! Note 
+that strings have an .includes method, too.
+*/
 
 function myIncludes(myArray, searchValue) {
   for(let i = 0; i < myArray.length; i++) {
@@ -15,6 +20,7 @@ function myIncludes(myArray, searchValue) {
   return false;
 }
 
-//We loop through the array searching each element to see if the value
-//matches searchValue and return true if it is. If not, the loop
-//is exited and returns false.
+/*
+We loop through the array searching each element to see if the 
+value matches searchValue and return true if it is. If not, the 
+loop is exited and returns false.
