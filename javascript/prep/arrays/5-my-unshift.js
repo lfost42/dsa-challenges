268aa9a5b4efa5b912da1829e62ea2cc9a0e3bc5
Lynda Foster <lfost42@wgu.edu>
2022-10-19 12:55:21 -0700

    Update 5-my-unshift.js

--- javascript/prep/arrays/5-my-unshift.js
+++ javascript/prep/arrays/5-my-unshift.js
@@ -1,11 +1,13 @@
-//Write a function myUnshift that takes an array and a value of any 
-//type as arguments.
+/*
+Write a function myUnshift that takes an array and a value of 
+any type as arguments.
 
-//myUnshift should return a new array, with value as the first 
-//element in the new array.
-//myUnshift([1, 2, 3], 0); // => [0, 1, 2, 3];
+myUnshift should return a new array, with value as the first 
+element in the new array.
+myUnshift([1, 2, 3], 0); // => [0, 1, 2, 3];
 
-//Do not use the built-in .unshift method for this problem!
+Do not use the built-in .unshift method for this problem!
+*/
 
 function myUnshift(inputArray, inputValue) {
   let newArray = [inputValue];
@@ -15,5 +17,8 @@ function myUnshift(inputArray, inputValue) {
   return newArray;
 }
 
-//We start by initiating newArray to aa array with inputValue as the
-//first item. The for loop then pushes the the inputArray onto newArray. 
+/*
+We start by initiating newArray to aa array with inputValue as 
+the first item. The for loop then pushes the the inputArray onto 
+newArray. 
+*/
