f60d692a1334a5bcb1e6c8d90e493d612513a777
Lynda Foster <lfost42@wgu.edu>
2022-10-24 06:50:18 -0700

    Update 6-even-and-odd.js

--- javascript/prep/arrays/6-even-and-odd.js
+++ javascript/prep/arrays/6-even-and-odd.js
@@ -1,11 +1,16 @@
-//Define a function, evenAndOdd, that accepts an array.
+/*
+Define a function, evenAndOdd, that accepts an array.
 
-//evenAndOdd should return a new array containing two smaller arrays. The first 
-//element in the new array should be an array with all of the even numbers from 
-//the original array. The second element in the new array should be an array with 
-//all of the odd numbers from the original array.
+evenAndOdd should return a new array containing two smaller 
+arrays. 
 
-//evenAndOdd([1, 2, 3, 4, 5, 6]); // => [[ 2 ,4, 6 ], [ 1, 3, 5 ]]
+The first element in the new array should be an array with all of 
+the even numbers from the original array. The second element in 
+the new array should be an array with all of the odd numbers from 
+the original array.
+
+evenAndOdd([1, 2, 3, 4, 5, 6]); // => [[ 2 ,4, 6 ], [ 1, 3, 5 ]]
+*/
 
 const evenAndOdd = (inputArray) == {
   let evenArray = [];
@@ -17,6 +22,8 @@ const evenAndOdd = (inputArray) == {
   return [evenArray, oddArray];
 }
 
-//We begin with two empty arrays. A for loop adds all even numbers to the 
-//evenArray and all odd numbers to the oddArray. We return an array
-//containing both the evenArray and oddArray. 
+/*
+We begin with two empty arrays. A for loop adds all even numbers 
+to the evenArray and all odd numbers to the oddArray. We return 
+an array containing both the evenArray and oddArray. 
+*/
