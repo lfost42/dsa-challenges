09c6d237d5dfce95a0c044872bcab9d099c3d694
Lynda Foster <lfost42@wgu.edu>
2022-11-02 21:51:35 -0700

    comp

--- python/arrayandstring/2-compress.py
+++ python/arrayandstring/2-compress.py
@@ -1,5 +1,6 @@
-/* PROBLEM */
-/*
+"""
+PROBLEM
+
 Write a function, compress, that takes in a string as an argument. The function 
 should return a compressed version of the string where consecutive occurrences 
 of the same characters are compressed into the number of occurrences followed by 
@@ -12,46 +13,47 @@ You can assume that the input only contains alphabetic characters.
 
 test_00:
 
-compress('ccaaatsss'); // -> '2c3at3s'
+compress('ccaaatsss') # -> '2c3at3s'
 test_01:
 
-compress('ssssbbz'); // -> '4s2bz'
+compress('ssssbbz') # -> '4s2bz'
 test_02:
 
-compress('ppoppppp'); // -> '2po5p'
+compress('ppoppppp') # -> '2po5p'
 test_03:
 
-compress('nnneeeeeeeeeeeezz'); // -> '3n12e2z'
+compress('nnneeeeeeeeeeeezz') # -> '3n12e2z'
 test_04:
 
 compress('yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy'); 
-// -> '127y'
-*/
+# -> '127y'
 
-/* SOLUTION */
-const compress = (s) => {
-	let result = [];
-	let i = 0;
-	let j = 0;
-	let num;
-	while (j <= s.length) {
-		if (s[i] === s[j]) {
-			j++;
-		} else {
-			num = j - i;
-			if (num === 1) {
-				result.push(s[i]);
-			} else {
-				result.push(num, s[i]);
-				i = j;
-			}
-		}
-	}
-	return result.join('');
-}
+SOLUTION
+"""
 
-/* EXPLANATION */
-/*
+def compress(s):
+  s += '!'
+  result = []
+  i = 0
+  j = 0
+  while j < len(s):
+    if s[i] == s[j]:
+      j += 1  
+    else:
+      num = j - i
+      if num == 1:
+        result.append(s[i])
+      else:
+        result.append(str(num)) 
+        result.append(s[i])
+      i = j
+    
+  return ''.join(result)
+
+
+"""
+EXPLANATION
+
 For this problem, we have groups of letter characters. Our goal is to return a 
 string that compresses each group of like-letter to a number representing the 
 number of letters there were in the input string followed by the letter. 
@@ -78,4 +80,4 @@ with other languages).
 To optimize the strategy to linear run time, we refactor the input to an empty 
 array and join the array into a string. Instead of concatenating the string, we 
 will push the characters into the array. 
-*/
+"""
